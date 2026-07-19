a65874ef5ab26bba9f11b245fa45db7337e7ea4f
satualdievaindira-beep <satualdievaindira@gmail.com>
2026-07-19 08:30:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
 
-# Расширенная база данных
+# База данных с твоими ссылками на постеры
 MOVIES = [
     {"id": 1, "title": "Аватар", "year": 2009, "author": "Джеймс Кэмерон", "desc": "История о далекой планете Пандора.", "poster": "https://kinogo.my/uploads/posts/2017-04/1493391756-1159271017-avatar.jpg", "rating": 7.9, "kp_url": "https://www.kinopoisk.ru/film/251733/"},
     {"id": 2, "title": "Властелин колец", "year": 2001, "author": "Питер Джексон", "desc": "Путешествие хоббита Фродо к Роковой горе.", "poster": "https://kinogo.my/uploads/posts/2019-07/1563720942-490328414-vlastelin-kolec-bratstvo-kolca.jpg", "rating": 8.6, "kp_url": "https://www.kinopoisk.ru/film/328/"},
     {"id": 3, "title": "Интерстеллар", "year": 2014, "author": "Кристофер Нолан", "desc": "Поиск нового дома для человечества в космосе.", "poster": "https://kinogo.my/uploads/posts/2017-04/1491114790-991695033-interstellar.jpg", "rating": 8.6, "kp_url": "https://www.kinopoisk.ru/film/258687/"},
     {"id": 4, "title": "Начало", "year": 2010, "author": "Кристофер Нолан", "desc": "Кража секретов через сны.", "poster": "https://kinogo.my/uploads/posts/2017-04/1491114986-2049908774-nachalo.jpg", "rating": 8.7, "kp_url": "https://www.kinopoisk.ru/film/447301/"},
-    {"id": 5, "title": "Темный рыцарь", "year": 2008, "author": "Кристофер Нолан", "desc": "Бэтмен против Джокера.", "poster": "https://kinogo.my/uploads/posts/2020-03/1585250490_the-dark-knight-2008.jpg", "rating": 8.5, "kp_url": "https://www.kinopoisk.ru/film/111543/"},
+    {"id": 5, "title": "Темный рыцарь", "year": 2008, "author": "Кристофер Нолан", "desc": "Бэтмен против Джокера.", "poster": "https://kinogo.my/uploads/posts/2020-04/1585997266-939135485-temnyy-rycar.jpg", "rating": 8.5, "kp_url": "https://www.kinopoisk.ru/film/111543/"},
     {"id": 6, "title": "Матрица", "year": 1999, "author": "Вачовски", "desc": "Мир — это компьютерная симуляция.", "poster": "https://kinogo.my/uploads/posts/2020-01/1578316075-753251593-matrica.jpg", "rating": 8.5, "kp_url": "https://www.kinopoisk.ru/film/301/"},
-    {"id": 7, "title": "Гладиатор", "year": 2000, "author": "Ридли Скотт", "desc": "История римского генерала.", "poster": "https://kinogo.my/uploads/posts/2019-11/1574343110_gladiator-2000.jpg", "rating": 8.6, "kp_url": "https://www.kinopoisk.ru/film/474/"},
+    {"id": 7, "title": "Гладиатор", "year": 2000, "author": "Ридли Скотт", "desc": "История римского генерала.", "poster": "https://kinogo.my/uploads/posts/2017-04/1493391510-611397561-gladiator.jpg", "rating": 8.6, "kp_url": "https://www.kinopoisk.ru/film/474/"},
     {"id": 8, "title": "Маска", "year": 1994, "author": "Чак Рассел", "desc": "Обычный клерк находит волшебную маску.", "poster": "https://kinogo.my/uploads/posts/2023-11/1699995824-1618804087-maska.jpg", "rating": 8.0, "kp_url": "https://www.kinopoisk.ru/film/2324/"},
     {"id": 9, "title": "Сияние", "year": 1980, "author": "Стэнли Кубрик", "desc": "Ужасы в уединенном отеле.", "poster": "https://kinogo.my/uploads/posts/2024-01/1704798751-1904935975-siyanie.jpg", "rating": 8.4, "kp_url": "https://www.kinopoisk.ru/film/356/"},
     {"id": 10, "title": "Гарри Поттер", "year": 2001, "author": "Крис Коламбус", "desc": "Мальчик узнает, что он волшебник.", "poster": "https://kinogo.my/uploads/posts/2019-07/1563015062-1572996915-garri-potter-i-filosofskiy-kamen.jpg", "rating": 8.2, "kp_url": "https://www.kinopoisk.ru/film/689/"},
     {"id": 11, "title": "Бойцовский клуб", "year": 1999, "author": "Дэвид Финчер", "desc": "Тайная организация бойцов.", "poster": "https://kinogo.my/uploads/posts/2017-04/1491114704-1886867375-boycovskiy-klub.jpg", "rating": 8.7, "kp_url": "https://www.kinopoisk.ru/film/361/"},
-    {"id": 12, "title": "Криминальное чтиво", "year": 1994, "author": "Квентин Тарантино", "desc": "Переплетение криминальных историй.", "poster": "https://kinogo.my/uploads/posts/2017-04/1491114844-1736636780-kriminalnoe-chtivo.jpg", "rating": 8.6, "kp_url": "https://www.kinopoisk.ru/film/342/"},
-    {"id": 13, "title": "Леон", "year": 1994, "author": "Люк Бессон", "desc": "История профессионального убийцы.", "poster": "https://kinogo.my/uploads/posts/2017-04/1493391484-904874384-leon.jpg", "rating": 8.7, "kp_url": "https://www.kinopoisk.ru/film/389/"},
-    {"id": 14, "title": "Зеленая миля", "year": 1999, "author": "Фрэнк Дарабонт", "desc": "Мистические события в тюрьме.", "poster": "https://kinogo.my/uploads/posts/2017-04/1491114878-1440786967-zelenaya-milya.jpg", "rating": 9.1, "kp_url": "https://www.kinopoisk.ru/film/435/"},
-    {"id": 15, "title": "Форрест Гамп", "year": 1994, "author": "Роберт Земекис", "desc": "Невероятная жизнь простого парня.", "poster": "https://kinogo.my/uploads/posts/2017-04/1491114949-1981507786-forrest-gamp.jpg", "rating": 8.9, "kp_url": "https://www.kinopoisk.ru/film/448/"}
+    {"id": 12, "title": "Криминальное чтиво", "year": 1994, "author": "Квентин Тарантино", "desc": "Переплетение криминальных историй.", "poster": "https://kinogo.my/uploads/posts/2023-11/1700692804-555184796-kriminalnoe-chtivo.jpg", "rating": 8.6, "kp_url": "https://www.kinopoisk.ru/film/342/"},
+    {"id": 13, "title": "Леон", "year": 1994, "author": "Люк Бессон", "desc": "История профессионального убийцы.", "poster": "https://kinogo.my/uploads/posts/2019-07/1564070804-298906622-leon.jpg", "rating": 8.7, "kp_url": "https://www.kinopoisk.ru/film/389/"},
+    {"id": 14, "title": "Зеленая миля", "year": 1999, "author": "Фрэнк Дарабонт", "desc": "Мистические события в тюрьме.", "poster": "https://kinogo.my/uploads/posts/2017-10/1509302130-598249484-zelenaya-milya.jpg", "rating": 9.1, "kp_url": "https://www.kinopoisk.ru/film/435/"},
+    {"id": 15, "title": "Форрест Гамп", "year": 1994, "author": "Роберт Земекис", "desc": "Невероятная жизнь простого парня.", "poster": "https://kinogo.my/uploads/posts/2020-02/1581515741-1303433223-forrest-gamp.jpg", "rating": 8.9, "kp_url": "https://www.kinopoisk.ru/film/448/"}
 ]
 
 def get_html(content):
